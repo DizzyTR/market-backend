ef2731ddbbe94bcb8e19b1f3d31cafded785bd7a
DizzyTR <99077264+DizzyTR@users.noreply.github.com>
2026-05-16 04:01:07 +0300

    Comment out MySQL connection configuration
    Comment out the MySQL connection code.

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ const db = mysql.createConnection({
 })
 
 /*const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT)
+})*/
+
+/*const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
