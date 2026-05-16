00d1b986435d92b3b74c5c592de7acd088885abf
DizzyTR <ardabeyx17@hotmail.com>
2026-05-16 03:31:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ app.use(cors())
 app.use(express.json())
 
 const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT)
+})
+
+/*const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
-})
+})*/
 
 db.connect((err) => {
   if (err) {
